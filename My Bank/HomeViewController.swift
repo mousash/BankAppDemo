ffcc1c3b27c7928564f1b831c17712c92865ced4
Mousa Sharifi <>
2021-06-03 13:06:56 +0200

    add homeVC

---
+++ My Bank/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  My Bank
+//
+//  Created by Mousa on 03.06.21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
